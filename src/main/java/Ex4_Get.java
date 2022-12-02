15b2539b601933af51334de86bc3c03ef89061e2
Alexey Mikson <alexey.mikson@gmail.com>
2022-12-02 16:06:45 +0300

    Exercise 4 Complete

---
+++ src/main/java/Ex4_Get.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+public class Ex4_Get {
+
+    @Test
+    public void getText() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
