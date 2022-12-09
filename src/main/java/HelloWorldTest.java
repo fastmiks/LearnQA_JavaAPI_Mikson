e2f3cca5639cb49c9037a15eb245fb69ab082439
Alexey Mikson <alexey.mikson@gmail.com>
2022-12-09 17:12:36 +0300

    Exercise 4 Complete

---
+++ src/main/java/HelloWorldTest.java
@@ -0,0 +1,17 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+
+    public void HelloWorldTest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
